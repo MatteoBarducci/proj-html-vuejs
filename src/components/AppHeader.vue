60f62b6788f870d3aa7a2d2367005c19a1f26fa0
MatteoBarducci <mbardu1301@gmail.com>
2024-04-23 13:46:17 +0200

    feat: add AppHeader

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -16,11 +16,17 @@
         <div class="header-left">
             <img src="../assets/logo.svg" alt="">
         </div>
-        <div class="header-centre">
-
-        </div>
+        <nav class="header-centre">
+            <div v-for="headerLink in store.headerInfo">
+                <a :href="headerLink.link">{{ headerLink.text }}</a>
+            </div>
+        </nav>
         <div class="header-right">
-
+            <i class="fa-solid fa-magnifying-glass"></i>
+            <div class="divider"></div>
+            <div v-for="social in store.headerSocials">
+                <a :href="social.link"> <i class="fa-brands" :class="social.icon"></i></a>
+            </div>
         </div>
     </header>
 </template>
@@ -34,10 +40,32 @@ header{
     align-items: center;
     height: 30px;
     padding: 25px;
-    border: dashed;
 
     img{
         height: 30px;
+    }
+
+    .header-centre{
+        display: flex;
+        gap: 30px;
+        align-items: center;
+
+    }
+    
+    a{
+        color: #0F0F0F;
+    }
+    .header-right{
+        display: flex;
+        gap: 20px;
+        align-items: center;
+
+
+        .divider{
+            height: 18px;
+            background-color: grey;
+            width: 1px;
+        }
     }
 }
 
--- src/store.js
+++ src/store.js
@@ -2,6 +2,52 @@ import { reactive } from "vue";
 
 export const store = reactive({
     headerInfo: [
-        
+        {
+            text: 'HOME',
+            link: '#'
+        },
+        {
+            text: 'BLOG',
+            link: '#'
+        },
+        {
+            text: 'PAGES ⌄',
+            link: '#'
+        },
+        {
+            text: 'ABOUT',
+            link: '#'
+        },
+        {
+            text: 'CONTACTS',
+            link: '#'
+        },
+        {
+            text: 'GALLERY',
+            link: '#'
+        },
+        {
+            text: 'SHOP ⌄',
+            link: '#'
+        },
+    ],
+    headerSocials:[
+        {
+            icon: 'fa-facebook',
+            link: 'https://facebook.com/'
+        },
+        {
+            icon: 'fa-twitter',
+            link: 'https://twitter.com/'
+        },
+        {
+            icon: 'fa-youtube',
+            link: 'https://youtube.com/'
+        },
+        {
+            icon: 'fa-instagram',
+            link: 'https://instagram.com/'
+        },
     ]
+
 });
